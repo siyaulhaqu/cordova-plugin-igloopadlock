907845b8c90fa2bddf2a11b2c6e3c9b0a36bd0f4
siyaulhaqu <siya@headfitted.com>
2025-05-29 01:01:32 +0200

    Update igloo.js

--- www/igloo.js
+++ www/igloo.js
@@ -1,16 +1,44 @@
 var exec = require('cordova/exec');
 
-module.exports = {
-  connect: function(bluetoothId, success, error) {
-    exec(success, error, "IglooPlugin", "connect", [bluetoothId]);
+var IglooPlugin = {
+  /**
+   * Connect to the lock with given Bluetooth ID.
+   * @param {string} bluetoothId - The Bluetooth MAC address of the lock.
+   * @param {function} success - Callback when connected.
+   * @param {function} error - Callback on error.
+   */
+  connect: function (bluetoothId, success, error) {
+    exec(success, error, 'IglooPlugin', 'connect', [bluetoothId]);
   },
-  unlockWithPin: function(data, success, error) {
-    exec(success, error, "IglooPlugin", "unlockWithPin", [data]);
+
+  /**
+   * Unlock the lock using unlock credentials (eKey).
+   * @param {object} data - The unlock data including pin, aesKey, lockData, lockVersion, timestamp.
+   * @param {function} success - Callback when unlocked successfully.
+   * @param {function} error - Callback on failure.
+   */
+  unlockWithPin: function (data, success, error) {
+    exec(success, error, 'IglooPlugin', 'unlockWithPin', [data]);
   },
-  readLogs: function(data, success, error) {
-    exec(success, error, "IglooPlugin", "readLogs", [data]);
+
+  /**
+   * Read the activity logs from the lock.
+   * @param {object} data - The log request data including aesKey, lockData, lockVersion, timestamp.
+   * @param {function} success - Callback with logs as array.
+   * @param {function} error - Callback on failure.
+   */
+  readLogs: function (data, success, error) {
+    exec(success, error, 'IglooPlugin', 'readLogs', [data]);
   },
-  disconnect: function(success, error) {
-    exec(success, error, "IglooPlugin", "disconnect", []);
+
+  /**
+   * Disconnect from the lock.
+   * @param {function} success - Callback when disconnected.
+   * @param {function} error - Callback on error.
+   */
+  disconnect: function (success, error) {
+    exec(success, error, 'IglooPlugin', 'disconnect', []);
   }
 };
+
+module.exports = IglooPlugin;
